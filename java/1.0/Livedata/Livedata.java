65eedc03edf1c276c1898926be6d6276fa2f4772
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-09-30 23:07:08 +0900

    Update Livedata.java

--- java/1.0/Livedata/Livedata.java
+++ java/1.0/Livedata/Livedata.java
@@ -1,16 +1,37 @@
 package Livedata;
 
+/**
+ * An class that has an initial value and automatically executes a observer function when the value changes.
+ * @param <T> instanceof Object
+ */
 public class Livedata<T> {
     private T value;
     private Observer<T> observer;
-
+    
+    /**
+     * get <b>value</b> of livedata.<br>
+     * @return value of livedata
+     */
     public T getValue() { return this.value; }
+    /**
+     * set <b>value</b> of livedata.<br>
+     * if data was changed, call <b>observer</b> function.<br>
+     * (Check with the <b>equals</b> method of the object)
+     * @param value T
+     * @return whether data was changed. 
+     * .</pre>
+     */
     public boolean setValue(T value) {
         if (this.value.equals(value)) return false;
         this.value = value;
         this.observer.func(this.value);
         return true;
     }
+    /**
+     * Registers an <b>observer</b> function to call when the value of livedata changes.<br>
+     * The observer function is written in the form of a <b>Lambda</b> function.
+     * @param observer T -> void
+     */
     public void observe(Observer<T> observer) { this.observer = observer; }
     public Livedata(T value) { this.value = value; }
 }

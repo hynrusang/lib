4edb8592b53ff691929d1740c79b8de51b132c5f
hynrusang <84229112+hynrusang@users.noreply.github.com>
2023-10-02 14:53:47 +0900

    Update MainActivity.java

--- java/src/MainActivity.java
+++ java/src/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity {
             );
     		
     		Html.onCreate("font: Arial, 24; windows: 1040, 720", first);
-    		Html.onCreate("font: Arial, 24; windows: 1040, 720", second);
     		break;
     	}
     }

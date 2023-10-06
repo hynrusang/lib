21c52e2f806350e3e41d2d72cec44148add18002
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-06 16:49:33 +0900

    Update Livedata.java

--- java/1.0/util/Livedata.java
+++ java/1.0/util/Livedata.java
@@ -5,35 +5,41 @@ package util;
  * @param <T> instanceof Object
  */
 public class Livedata<T> {
+	private T value;
+	private Observer<T> observer;
+	
 	public static interface Observer<T> {
-		void function(T it);
+		void launch(T it);
 	}
-    private T value;
-    private Observer<T> observer;
-    /**
-     * get <b>value</b> of livedata.<br>
-     * @return value of livedata
-     */
-    public T getValue() { return this.value; }
-    /**
-     * set <b>value</b> of livedata.<br>
-     * if data was changed, call <b>observer</b> function.<br>
-     * (Check with the <b>equals</b> method of the object)
-     * @param value new value to set
-     * @return whether data was changed. 
-     * .</pre>
-     */
-    public boolean setValue(T value) {
-        if (this.value.equals(value)) return false;
-        this.value = value;
-        this.observer.function(this.value);
-        return true;
-    }
-    /**
-     * Registers an <b>observer</b> function to call when the value of livedata changes.<br>
-     * The observer function is written in the form of a <b>Lambda</b> function.
-     * @param observer Lambda function to run automatically when the value changes
-     */
-    public void observe(Observer<T> observer) { this.observer = observer; }
-    public Livedata(T value) { this.value = value; }
+	
+	/**
+         * get <b>value</b> of livedata.<br>
+         * @return value of livedata
+         */
+	public T getValue() { return this.value; }
+	/**
+         * set <b>value</b> of livedata.<br>
+	 * if data was changed, call <b>observer</b> function.<br>
+         * (Check with the <b>equals</b> method of the object)
+	 * @param value new value to set
+         * @return whether data was changed. 
+	 * .</pre>
+         */
+	public boolean setValue(T value) {
+		if (this.value.equals(value)) return false;
+		this.value = value;
+		this.observer.launch(this.value);
+		return true;
+	}
+	/**
+         * Registers an <b>observer</b> function to call when the value of livedata changes.<br>
+	 * The observer function is written in the form of a <b>Lambda</b> function.
+         * @param observer Lambda function to run automatically when the value change.
+	 */
+	public void observe(Observer<T> observer) { 
+		this.observer = observer; 
+	}
+	public Livedata(T value) {
+		this.value = value; 
+	}
 }

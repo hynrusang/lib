f2755dc4a1d6d538ae977a25a167b3a7281f8648
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-07 03:01:55 +0900

    Update Button.java

--- java/1.0/document/Button.java
+++ java/1.0/document/Button.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 final public class Button extends HTMLElement<Button> {
 	/**
 	 * Registers the listener that runs when you click a button.
-	 * @param listener listener to run when a button is clicked
+	 * @param listener Listener to run when a button is clicked
 	 * @return This element for the chaining method
 	 */
 	public Button onClick(ActionListener listener) {

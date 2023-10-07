ea09900ba10b92b53873ab8a726ca1dcb8773479
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-07 18:33:42 +0900

    Update R.java

--- java/src/R.java
+++ java/src/R.java
@@ -1,18 +1,18 @@
 import java.awt.Color;
-import javax.swing.JTextField;
 import document.*;
 
 public class R {
 	public static Fragment activity_main = new Fragment(
-		new Div().size(100, 0, 0, 60).background(Color.RED),
-		new Form(
-			new Input("placeholder").size(50, 0, 0, 40).position(25, 0, 0, 40),
-			new Button("submit").size(0, 100, 0, 40).position(50, -50, 0, 80)
-		).size(100, 0, 100, -120).identity("true").position(0, 0, 0, 60).background(Color.YELLOW).onSubmit(e -> {
-			for (HTMLElement<?> field: e.getFormData()) {
-				if (field instanceof Input) System.out.println(((Input)field).getValue());
-			}
-		}),
+		new Div(
+			new Form(
+				new Input("placeholder").size(50, 0, 0, 40).position(25, 0, 0, 40),
+				new Button("submit").size(0, 100, 0, 40).position(50, -50, 0, 80)
+			).size(100, 0, 100, 0).background(Color.YELLOW).onSubmit(e -> {
+				for (HTMLElement<?> field: e.getFormData()) {
+					if (field instanceof Input) System.out.println(((Input)field).getValue());
+				}
+			})
+		).size(100, 0, 100, -60),
 		new Div(
 			new Button("first").size(20, 0, 100, 0).onClick(e -> {
 				Html.replace(R.activity_main);

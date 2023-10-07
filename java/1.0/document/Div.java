7fbf31c04a48a461b11a54813defffc8d878c399
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-07 23:08:00 +0900

    Update Div.java

--- java/1.0/document/Div.java
+++ java/1.0/document/Div.java
@@ -1,20 +1,8 @@
 package document;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneLayout;
 
-class DivInner extends HTMLElement<DivInner> {
-	public DivInner(HTMLElement<?>... elements) {
-		super(new JPanel(), elements);
-	}
-}
 final public class Div extends HTMLElement<Div> {
-	private DivInner inner;
 	public Div(HTMLElement<?>... elements) {
-		super(new JScrollPane());
-		inner = new DivInner(elements);
-		border(null, 0);
-		((JScrollPane)main).setViewportView(inner.main);
-		((JScrollPane)main).setLayout(new ScrollPaneLayout());
+		super(new JPanel(), elements);
 	}
 }

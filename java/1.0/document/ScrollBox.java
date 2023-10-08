c56fe0021c2f18075a1a39531eab26d91e8cabf1
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-08 10:59:03 +0900

    Update ScrollBox.java

--- java/1.0/document/ScrollBox.java
+++ java/1.0/document/ScrollBox.java
@@ -1,5 +1,10 @@
 package document;
 
-public class ScrollBox {
-
+public class ScrollBox extends HTMLElement<ScrollBox> {
+    private JPanel mainPanel;
+    public ScrollBox(int shellCount, HTMLElement<?>... elements) {
+        super(new JScrollPane());
+        // main.setLayout(new ScrollPaneLayout());
+        mainPanel = new JPanel();
+    }
 }

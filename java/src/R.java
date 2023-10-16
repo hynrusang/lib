6ce5880150b63b30af047c04457318f32d2368cf
hynrusang <84229112+hynrusang@users.noreply.github.com>
2023-10-16 15:52:47 +0900

    Update R.java

--- java/src/R.java
+++ java/src/R.java
@@ -10,7 +10,7 @@ public class R {
 			new Button("second").size(20, 0, 100, 0).position(20, 0, 0, 0).onClick(e -> {
 				Html.navigate(1);
 			})
-		).size(100, 0, 0, 60).position(0, 0, 100, -60);
+		).size(100, 0, 0, 40).position(0, 0, 100, -40);
 	}
 	
 	public static Fragment activity_main = new Fragment(
@@ -28,15 +28,15 @@ public class R {
 			new Div().size(100, 0, 0, 100).background(Color.YELLOW).position(0, 0, 0, 320),
 			new Div().size(100, 0, 0, 100).background(Color.GREEN).position(0, 0, 0, 420),
 			new Div().size(100, 0, 0, 100).background(Color.BLUE).position(0, 0, 0, 520)
-		).size(100, 0, 100, -60),
+		).size(100, 0, 100, -40),
 		R.footer()
 	);
 	
 	public static Fragment activity_second = new Fragment("index 2",
-		new Div().size(100, 0, 0, 60).background(Color.RED),
+		new Div().size(100, 0, 0, 40).background(Color.RED),
 		new Div(
 			new Button("hello, world 2!").size(50, 0, 50, 0).position(25, 0, 25, 0)
-		).size(100, 0, 100, -120).position(0, 0, 0, 60).background(Color.YELLOW),
+		).size(100, 0, 100, -80).position(0, 0, 0, 40).background(Color.YELLOW),
 		R.footer()
 	);
 }

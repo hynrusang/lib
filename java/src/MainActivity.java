6cf65bb1484bdcaf0ac0c5157b972f675e50ba80
hynrusang <84229112+hynrusang@users.noreply.github.com>
2023-10-16 15:32:07 +0900

    up

--- java/src/MainActivity.java
+++ java/src/MainActivity.java
@@ -1,67 +1,21 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import view.Activity;
-import view.Fragment;
+import util.Livedata;
+import document.Html;
 
-public class MainActivity extends Activity {
-	public MainActivity() {
-		super("title: first activity",
-				new Fragment()
-		);
-		JPanel topPanel = new JPanel();
-		top(topPanel);
-		
-		JPanel scrollPanel = new JPanel();
-        scrollPanel.setLayout(new BoxLayout(scrollPanel, BoxLayout.Y_AXIS));
-        JScrollPane scrollPane = new JScrollPane(scrollPanel);
-        scrollPane.getVerticalScrollBar().setUnitIncrement(20);
-        scrollPane.getHorizontalScrollBar().setUnitIncrement(20);
-        
-        main.add(scrollPane, BorderLayout.CENTER);
-        
-        for (int i = 0; i < 100; i++) {
-            JPanel panel = new JPanel();
-            panel.setLayout(new BorderLayout());
-
-            // JLabel 추가
-            JLabel label = new JLabel("Label " + i);
-            panel.add(label, BorderLayout.WEST);
-
-            // JButton 추가
-            JButton button1 = new JButton("Button");
-            button1.setPreferredSize(new Dimension(80, 40)); // 40x100%
-            button1.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					intent(R.activity_second, "hello, world!");
-				}
-            });
-
-            // JPanel에 버튼 추가
-            JPanel buttonPanel = new JPanel();
-            buttonPanel.setLayout(new GridLayout(1, 2));
-            buttonPanel.add(button1);
-
-            panel.add(buttonPanel, BorderLayout.EAST);
-
-            scrollPanel.add(panel);
-        }
-        
-        bottom(new JPanel());
-	}
-
-	@Override
-	protected void handlingIntent() {
-		// TODO Auto-generated method stub
-		
-	}
-
+public class MainActivity {
+	private static String target = "Document";
+	public static void main(String[] args) {
+    	switch (MainActivity.target) {
+    	case "Livedata":
+    		Livedata<Integer> livedata = new Livedata<Integer>(0);
+    		livedata.observe(it -> System.out.println("data was changed:" + it));
+    		livedata.setValue(3);
+    		livedata.setValue(7);
+    		livedata.setValue(7);
+    		livedata.setValue(9);
+    		break;
+    	case "Document":
+    		Html.init(R.activity_main, R.activity_second);
+    		break;
+    	}
+    }
 }
--- java/src/R.java
+++ java/src/R.java
@@ -1,9 +1,37 @@
-import view.Activity;
+import java.awt.Color;
+import document.*;
 
 public class R {
-	public static Activity activity_main = new MainActivity().launch();
-	public static Activity activity_second = new SecondActivity();
-	public static void main(String[] args) {
-		
+	public static Div footer() {
+		return new Div(
+			new Button("first").size(20, 0, 100, 0).onClick(e -> {
+				Html.navigate(0);
+			}),
+			new Button("second").size(20, 0, 100, 0).position(20, 0, 0, 0).onClick(e -> {
+				Html.navigate(1);
+			})
+		).size(100, 0, 0, 60).position(0, 0, 100, -60);
 	}
+	
+	public static Fragment activity_main = new Fragment(
+		new Div(
+			new Form(
+				new Input("placeholder").size(50, 0, 0, 40).position(25, 0, 0, 20),
+				new Submit().size(0, 100, 0, 40).position(50, -50, 0, 60)
+			).size(100, 0, 0, 120).background(Color.YELLOW).onSubmit(e -> {
+				for (HTMLElement<?> field: e.getFormData()) {
+					System.out.println(field.getText());
+				}
+			})
+		).size(100, 0, 100, -60),
+		R.footer()
+	);
+	
+	public static Fragment activity_second = new Fragment("index 2",
+		new Div().size(100, 0, 0, 60).background(Color.RED),
+		new Div(
+			new Button("hello, world 2!").size(50, 0, 50, 0).position(25, 0, 25, 0)
+		).size(100, 0, 100, -120).position(0, 0, 0, 60).background(Color.YELLOW),
+		R.footer()
+	);
 }

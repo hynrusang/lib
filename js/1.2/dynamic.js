d435c971d1ae69f6ac4cf9d27f8676716389c82d
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-11-29 15:32:47 +0900

    Update dynamic.js

--- js/1.2/dynamic.js
+++ js/1.2/dynamic.js
@@ -19,7 +19,7 @@ const Dom = class {
                 if (["innerHTML", "html"].includes(key)) this.#node.innerHTML = value
                 else if (["innerText", "text"].includes(key)) this.#node.innerText = value
                 else if (!key.indexOf("on") || key == "async" || (this.#node.nodeName == "TEXTAREA" && key == "value")) this.#node[key] = value
-                else if (value) this.#node.setAttribute(key, value);
+                else if (typeof value != "undefined") this.#node.setAttribute(key, value);
             }
         } else throw new TypeError('Additional parameter must be an {key: value} object');
         return this;

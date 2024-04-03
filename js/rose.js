4b0295874c0b6f643e9bbb21385546bb28f92281
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-04-03 15:31:11 +0900

    Update rose.js

--- js/rose.js
+++ js/rose.js
@@ -1,3 +1,31 @@
+﻿/*
+rose library
+info:
+This library can download other JavaScript by module programs from my website.
+It was very simple and powerful to use.
+
+How it works:
+Inside the script tag to your site by type=module.
+Also, write in the script similar to the following.
+
+// ex: rose.js
+import getm from "https://hynrusang.github.io/lib/js/rose.js";
+
+const dynamic = getm("dynamic", "prerelease");
+...
+
+export { dynamic, ... }
+//
+
+Then, to access the module imported into rose, you can do the following.
+
+// ex: other.js
+import dynamic from "./rose.js";
+dynamic.Fragment(...);
+//
+
+Whole picture:
+*/
 const versionInfo = {};
 const versionParser = version => {
 }

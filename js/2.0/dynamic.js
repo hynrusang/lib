4dd6800ab04037a1e9165a5efd5aec434256bdfe
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-07-30 21:04:36 +0900

    Update dynamic.js

--- js/2.0/dynamic.js
+++ js/2.0/dynamic.js
@@ -39,7 +39,7 @@ const FragDom = class {
      * @type {(...dom: FragDom) => FragDom}
      */
     add = (...dom) => {
-        if (dom[0]) for (let pdom of dom.flat()) this.#node.appendChild(pdom.node);
+        if (dom.flat()[0]) for (let pdom of dom.flat()) this.#node.appendChild(pdom.node);
         return this;
     }
     /**

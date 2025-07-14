42ccc28e746ca7cd9a062475f37c945eb17dadd9
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-07-14 12:23:34 +0900

    Update dynamic.js

--- js/2.1/dynamic.js
+++ js/2.1/dynamic.js
@@ -27,14 +27,14 @@ const DocumentContainer = class {
      * @description Sets various properties or attributes of the DOM element.
      */
     set = additional => {
-        if (typeof additional === 'object') {
-            for (const [key, value] of Object.entries(additional)) {
-                if (["innerHTML", "html"].includes(key)) this.#node.innerHTML = value
-                else if (["innerText", "text"].includes(key)) this.#node.innerText = value
-                else if (!key.indexOf("on") || key == "async" || (this.#node.nodeName == "TEXTAREA" && key == "value")) this.#node[key] = value
-                else if (typeof value != "undefined") this.#node.setAttribute(key, value);
-            }
-        } else throw new TypeError('Additional parameter must be an {key: value} object');
+        if (typeof additional !== 'object') throw new TypeError("set() requires an object.");
+
+        for (const [key, value] of Object.entries(additional)) {
+            if (["innerHTML", "html"].includes(key)) this.#node.innerHTML = value
+            else if (["innerText", "text"].includes(key)) this.#node.innerText = value
+            else if (!key.indexOf("on") || key == "async" || (this.#node.nodeName == "TEXTAREA" && key == "value")) this.#node[key] = value
+            else if (typeof value != "undefined") this.#node.setAttribute(key, value);
+        }
         return this;
     };
 
@@ -44,8 +44,7 @@ const DocumentContainer = class {
      */
     reset = (...dom) => {
         this.#node.innerHTML = "";
-        this.add(...dom);
-        return this;
+        return this.add(...dom);;
     }
 
     /**
@@ -78,7 +77,11 @@ const $ = (node, additional) => new DocumentContainer(node, additional);
  * (selector: HTMLElement) => HTMLElement
  * }}
  */
-const scan = selector => (typeof selector == "string") ? (selector[0] == "!") ? document.querySelectorAll(selector.split("!")[1]) : document.querySelector(selector) : selector;
+const scan = selector => (typeof selector == "string") 
+    ? (selector[0] == "!") 
+        ? document.querySelectorAll(selector.split("!")[1]) 
+        : document.querySelector(selector) 
+    : selector;
 
 /**
  * @type {{
@@ -88,9 +91,8 @@ const scan = selector => (typeof selector == "string") ? (selector[0] == "!") ? 
  * }}
  */
 const snipe = selector => {
-   const temp = ((typeof selector == "string") && (selector[0] == "!")) ? [] : $(scan(selector));
-   if (Array.isArray(temp)) for (let i = 0; i < scan(selector).length; i++) temp.push($(scan(selector)[i]));
-   return temp;
+    if (typeof selector === "string" && selector.startsWith("!")) return Array.from(scan(selector)).map(el => $(el));
+    return $(scan(selector));
 }
 /**
  * @type {() => null}
@@ -107,15 +109,19 @@ const help = () => {
 
 const Fragment = class {
     static #animation = class {
-        static #applyAnimation = async (fragment, animationIn, animationOut) => {
+        static async #applyAnimation(fragment, animationIn, animationOut) {
             const view = snipe(fragment.#view);
-            if (view.node.innerHTML != "") {
-                await view.node.animate(animationIn, {duration: fragment.#animationExcuteTime * 0.5}).finished;
+            const animationDuration = { duration: fragment.#animationExcuteTime * 0.5 };
+
+            if (view.node.innerHTML) {
+                await view.node.animate(animationIn, animationDuration).finished();
                 view.reset(fragment.#domlist);
-                view.node.animate(animationOut, {duration: fragment.#animationExcuteTime * 0.5});
+                view.node.animate(animationOut, animationDuration);
             } else view.reset(fragment.#domlist);
-            if (typeof fragment.#action === "function") fragment.#action();
-        };
+            
+            if (typeof fragment.#action === 'function') fragment.#action();
+        }
+
         static card = async fragment => await this.#applyAnimation(fragment, [
             {transform: 'rotateY(0deg)', opacity: '1'}, 
             {transform: 'rotateY(180deg)', opacity: '0'}
@@ -123,6 +129,7 @@ const Fragment = class {
             {transform: 'rotateY(180deg)', opacity: '0'}, 
             {transform: 'rotateY(360deg)', opacity: '1'}
         ]);
+
         static fade = async fragment => await this.#applyAnimation(fragment, [
             {opacity: '1'}, 
             {opacity: '0'}
@@ -130,6 +137,7 @@ const Fragment = class {
             {opacity: '0'}, 
             {opacity: '1'}
         ]);
+
         static swip = async fragment => await this.#applyAnimation(fragment, [
             { left: '0px', position: 'relative' }, 
             { left: '100vw', position: 'relative' }
@@ -138,6 +146,7 @@ const Fragment = class {
             { left: '0px', position: 'relative' }
         ]);
     }
+
     #rid;
     #view;
     #domlist;
@@ -149,32 +158,28 @@ const Fragment = class {
      * @type {(arg: any) => Fragment}
      */
     launch = arg => {
-        if (this.#swipAnimation != null) Fragment.#animation[this.#swipAnimation](this);
+        if (this.#swipAnimation) Fragment.#animation[this.#swipAnimation](this);
         else {
             snipe(this.#view).reset(...this.#domlist)
             if (typeof this.#action == "function") this.#action(arg);
         }
         return this;
     }
+
     /**
      * @type {(action: Function) => Fragment}
      */
-    registAction = action => {
-        this.#action = action;
-        return this;
-    }
+    registAction = action => (this.#action = action, this);
+
     /**
      * @type {(animation: string, second: Number) => Fragment}
      */
-    registAnimation = (animation, second) => {
-        this.#swipAnimation = animation;
-        this.#animationExcuteTime = second;
-        return this;
-    }
+    registAnimation = (animation, second) => (this.#swipAnimation = animation, this.#animationExcuteTime = second, this);
 
     get rid() {
         return this.#rid;
     }
+
     /**
      * @type {(view: String, ...domlist: DocumentContainer | DocumentContainer[]) => Fragment}
      */
@@ -196,21 +201,25 @@ const FragMutation = class {
      */
     static mutate = (fragment, arg, alwayRefresh = false) => {
         const router = scan("router");
+
         if (!scan(`fragment[rid=${fragment.rid}]`)) {
-            snipe("fragmentbox").add($("fragment", {rid: fragment.rid}));
+            snipe("fragmentbox").add($("fragment", { rid: fragment.rid }));
             fragment.launch(arg);
             this.#launchedInfo.fragments[fragment.rid] = fragment;
         } else if (alwayRefresh || this.#launchedInfo.target == fragment.rid) {
             fragment.launch(arg);
             this.#launchedInfo.fragments[fragment.rid] = fragment;
         };
+
         if (this.#launchedInfo.target != fragment.rid) {
-            scan("!fragmentbox fragment").forEach(node => node.style.display = "none");
+            scan("!fragmentbox fragment").forEach(el => el.style.display = "none");
             scan(`fragment[rid=${fragment.rid}]`).style.display = null;
+
             if (router) snipe(router).reset(this.#launchedInfo.router[fragment.rid]);
             this.#launchedInfo.target = fragment.rid;
         }
     };
+
     /**
      * @type {(rid: String, domlist: DocumentContainer[]) => void}
      */
@@ -218,14 +227,17 @@ const FragMutation = class {
         this.#launchedInfo.router[rid] = domlist;
         if (this.#launchedInfo.target == rid) snipe("router").reset(domlist)
     }
+
     /**
      * @type {() => void}
      */
     static refresh = () => {
-        const nodeList = Array.from(scan(`!fragment[rid='${this.#launchedInfo.fragments[this.#launchedInfo.target].rid}'] *`))
-        const temp = nodeList.map(element => element.scrollTop)
-        this.#launchedInfo.fragments[this.#launchedInfo.target].launch();
-        nodeList.forEach((element, index) => element.scrollTop = temp[index]);
+        const currentRid = this.#launchedInfo.target;
+        const elements = Array.from(scan(`!fragment[rid='${currentRid}'] *`));
+        const scrolls = elements.map(el => el.scrollTop);
+
+        this.#launchedInfo.fragments[currentRid].launch();
+        elements.forEach((el, i) => el.scrollTop = scrolls[i]);
     }
 }
 
